12950c85841d9b58123f08df3608ca84780d3a1d
zcdu <zcdu@foxmail.com>
2024-12-03 20:37:42 +0800

    refactor(server): 增加插件

---
+++ README.md
@@ -0,0 +1,25 @@
+# 当前用到的仓库
+https://github.com/chrisgoringe/cg-use-everywhere.git
+https://github.com/sipherxyz/comfyui-art-venture.git
+https://github.com/kijai/ComfyUI-CogVideoXWrapper.git
+https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git
+https://github.com/huggingface/controlnet_aux.git
+https://github.com/crystian/ComfyUI-Crystools.git
+https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
+https://github.com/lldacing/comfyui-easyapi-nodes.git
+https://github.com/yolain/ComfyUI-Easy-Use.git
+https://github.com/cubiq/ComfyUI_essentials.git
+https://github.com/kijai/ComfyUI-FluxTrainer.git
+https://github.com/ali-vilab/In-Context-LoRA.git
+https://github.com/kijai/ComfyUI-KJNodes.git
+https://github.com/heshengtao/comfyui_LLM_party.git
+https://github.com/ltdrdata/ComfyUI-Manager.git
+https://github.com/shadowcz007/comfyui-mixlab-nodes.git
+https://github.com/sipie800/ComfyUI-PuLID-Flux-Enhanced.git
+https://github.com/kijai/ComfyUI-PyramidFlowWrapper.git
+https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git
+https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
+https://github.com/rgthree/rgthree-comfy.git
+https://github.com/WASasquatch/was-node-suite-comfyui.git
+https://github.com/XLabs-AI/x-flux-comfyui.git
+
---
+++ install.sh
@@ -0,0 +1,27 @@
+git clone https://github.com/comfyanonymous/ComfyUI.git
+git clone https://github.com/chrisgoringe/cg-use-everywhere.git ComfyUI/custom_nodes/cg-use-everywhere
+git clone https://github.com/sipherxyz/comfyui-art-venture.git ComfyUI/custom_nodes/comfyui-art-venture
+git clone https://github.com/kijai/ComfyUI-CogVideoXWrapper.git ComfyUI/custom_nodes/ComfyUI-CogVideoXWrapper
+git clone https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git ComfyUI/custom_nodes/ComfyUI_Comfyroll_CustomNodes
+git clone https://github.com/crystian/ComfyUI-Crystools.git ComfyUI/custom_nodes/ComfyUI-Crystools
+git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git ComfyUI/custom_nodes/ComfyUI-Custom-Scripts
+git clone https://github.com/lldacing/comfyui-easyapi-nodes.git ComfyUI/custom_nodes/comfyui-easyapi-nodes
+git clone https://github.com/yolain/ComfyUI-Easy-Use.git ComfyUI/custom_nodes/ComfyUI-Easy-Use
+git clone https://github.com/cubiq/ComfyUI_essentials.git ComfyUI/custom_nodes/ComfyUI_essentials
+git clone https://github.com/Fannovel16/comfyui_controlnet_aux.git ComfyUI/custom_nodes/comfyui_controlnet_aux
+git clone https://github.com/kijai/ComfyUI-FluxTrainer.git ComfyUI/custom_nodes/ComfyUI-FluxTrainer
+git clone https://github.com/ali-vilab/In-Context-LoRA.git ComfyUI/custom_nodes/In-Context-LoRA
+git clone https://github.com/kijai/ComfyUI-KJNodes.git ComfyUI/custom_nodes/ComfyUI-KJNodes
+git clone https://github.com/heshengtao/comfyui_LLM_party.git ComfyUI/custom_nodes/comfyui_LLM_party
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git ComfyUI/custom_nodes/ComfyUI-Manager
+git clone https://github.com/shadowcz007/comfyui-mixlab-nodes.git ComfyUI/custom_nodes/comfyui-mixlab-nodes
+git clone https://github.com/sipie800/ComfyUI-PuLID-Flux-Enhanced.git ComfyUI/custom_nodes/ComfyUI-PuLID-Flux-Enhanced
+git clone https://github.com/kijai/ComfyUI-PyramidFlowWrapper.git ComfyUI/custom_nodes/ComfyUI-PyramidFlowWrapper
+git clone https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git ComfyUI/custom_nodes/ComfyUI_UltimateSDUpscale
+git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git ComfyUI/custom_nodes/ComfyUI-VideoHelperSuite
+git clone https://github.com/rgthree/rgthree-comfy.git ComfyUI/custom_nodes/rgthree-comfy
+git clone https://github.com/WASasquatch/was-node-suite-comfyui.git ComfyUI/custom_nodes/was-node-suite-comfyui
+git clone https://github.com/XLabs-AI/x-flux-comfyui.git ComfyUI/custom_nodes/x-flux-comfyui
+git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack.git ComfyUI/ComfyUI-Impact-Pack
+git clone https://github.com/lrzjason/Comfyui-In-Context-Lora-Utils.git ComfyUI/Comfyui-In-Context-Lora-Utils
+docker build -t comfyui:0.2.41 .
